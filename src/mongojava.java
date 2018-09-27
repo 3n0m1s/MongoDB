e5ae81357af3e19fd8b3d4dea4d1758c9064ccba
Simone Schiavolini <simone.schiavolini@progettispeciali.net>
2018-09-28 00:42:02 +0200

    Add files via upload

---
+++ src/mongojava.java
@@ -0,0 +1,25 @@
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class mongojava {
+
+	public static void main(String[] args) {
+		System.out.println("Start db");
+		connect();
+	}
+	
+	public static void connect() {
+	
+		 MongoClientURI connectionString = new MongoClientURI("mongodb://mimonenew:mimonenewpass@www.epocum.com:27017");
+		 MongoClient mongo = new MongoClient(connectionString);
+		 MongoDatabase database = mongo.getDatabase("admin");
+		 MongoCollection<Document> collection = database.getCollection("persone");
+		 Document obj = collection.find().first();
+		 System.out.println(obj.toJson());
+		 mongo.close();
+	}
+
+}
